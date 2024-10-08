e5a1bdd3c63de5b69dc0a45d93cbbb62e8583d5f
Phinocio <github@phinocio.com>
2024-10-08 17:08:00 -0600

    refactor: move routes files around

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        api: __DIR__ . '/../routes/api/base.php',
+        commands: __DIR__ . '/../routes/console/base.php',
         health: '/up',
         apiPrefix: '',
     )
