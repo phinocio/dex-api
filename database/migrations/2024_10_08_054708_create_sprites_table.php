587740dfac08a41f322eb7d1f2dd5ad8b778fcca
Phinocio <github@phinocio.com>
2024-10-08 15:06:30 -0600

    fix: typo

--- database/migrations/2024_10_08_054708_create_sprites_table.php
+++ database/migrations/2024_10_08_054708_create_sprites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sprites', function (Blueprint $table) {
             $table->id();
             $table->string('url')->unique();
-            $table->enum('verison', [
+            $table->enum('version', [
                 'red-blue',
                 'yellow',
                 'gold',
