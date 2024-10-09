5317833c19aa6cba1e96f93133d239e399b61841
Phinocio <github@phinocio.com>
2024-10-09 13:00:45 -0600

    feat: add gen 3 and 4 pokemon

--- database/seeders/PokemonSeeder.php
+++ database/seeders/PokemonSeeder.php
@@ -6,6 +6,8 @@ namespace Database\Seeders;
 
 use App\Enums\Gen1Pokemon;
 use App\Enums\Gen2Pokemon;
+use App\Enums\Gen3Pokemon;
+use App\Enums\Gen4Pokemon;
 use App\Enums\Generation;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,11 +20,10 @@ class PokemonSeeder extends Seeder
         DB::table('pokemon')->insert(
             collect(Gen1Pokemon::cases())->map(function (Gen1Pokemon $pokemon) {
                 return [
-                    'id' => $pokemon->value,
                     'name' => $pokemon->name(),
                     'slug' => $pokemon->slug(),
                     'generation_id' => Generation::I,
-                    'national_dex_number' => $pokemon->value, // DOESNT WORK ONCE I ADD FORMS
+                    'national_dex_number' => $pokemon->value,
                 ];
             })->toArray(),
         );
@@ -30,11 +31,32 @@ class PokemonSeeder extends Seeder
         DB::table('pokemon')->insert(
             collect(Gen2Pokemon::cases())->map(function (Gen2Pokemon $pokemon) {
                 return [
-                    'id' => $pokemon->value,
                     'name' => $pokemon->name(),
                     'slug' => $pokemon->slug(),
                     'generation_id' => Generation::II,
-                    'national_dex_number' => $pokemon->value, // DOESNT WORK ONCE I ADD FORMS
+                    'national_dex_number' => $pokemon->value,
+                ];
+            })->toArray(),
+        );
+
+        DB::table('pokemon')->insert(
+            collect(Gen3Pokemon::cases())->map(function (Gen3Pokemon $pokemon) {
+                return [
+                    'name' => $pokemon->name(),
+                    'slug' => $pokemon->slug(),
+                    'generation_id' => Generation::III,
+                    'national_dex_number' => $pokemon->value,
+                ];
+            })->toArray(),
+        );
+
+        DB::table('pokemon')->insert(
+            collect(Gen4Pokemon::cases())->map(function (Gen4Pokemon $pokemon) {
+                return [
+                    'name' => $pokemon->name(),
+                    'slug' => $pokemon->slug(),
+                    'generation_id' => Generation::IV,
+                    'national_dex_number' => $pokemon->value,
                 ];
             })->toArray(),
         );
