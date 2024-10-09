97415f74cc028e132efec938de17835ca6594877
Phinocio <github@phinocio.com>
2024-10-08 21:00:00 -0600

    refactor: add names to routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Api\v1\DexController;
 use App\Http\Controllers\Api\v1\GameController;
+use App\Http\Controllers\Api\v1\GameDexController;
 use App\Http\Controllers\Api\v1\GenerationController;
 use App\Http\Controllers\Api\v1\PokemonController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/generations', [GenerationController::class, 'index']);
-Route::get('/generations/{param}', [GenerationController::class, 'show']);
+Route::get('/generations', [GenerationController::class, 'index'])->name('generations.index');
+Route::get('/generations/{param}', [GenerationController::class, 'show'])->name('generations.show');
 
-Route::get('/games', [GameController::class, 'index']);
-Route::get('/games/{param}', [GameController::class, 'show']);
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
+Route::get('/games/{param}', [GameController::class, 'show'])->name('games.show');
 
-Route::get('/dexes', [DexController::class, 'index']);
-Route::get('/dexes/{param}', [DexController::class, 'show']);
+Route::get('/game-dexes', [GameDexController::class, 'index'])->name('game_dexes.index');
+Route::get('/game-dexes/{param}', [GameDexController::class, 'show'])->name('game_dexes.show');
 
-Route::get('/pokemon', [PokemonController::class, 'index']);
-Route::get('/pokemon/{param}', [PokemonController::class, 'show']);
+Route::get('/pokemon', [PokemonController::class, 'index'])->name('pokemon.index');
+Route::get('/pokemon/{param}', [PokemonController::class, 'show'])->name('pokemon.show');
