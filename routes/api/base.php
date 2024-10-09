2f2d5abf3c3fd7427d4f18221c03aad54b038bf3
Phinocio <github@phinocio.com>
2024-10-08 22:59:49 -0600

    refactor: use v1 subfolder for api routes
    Might eventually have routes split up into individual files, this
    "future proofs" that

--- routes/api/base.php
+++ routes/api/base.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path('routes/api/v1.php'));
+Route::prefix('v1')->group(base_path('routes/api/v1/routes.php'));
