eb57397c4c404cc026e3d78baa13c0f89c90d18d
Phinocio <github@phinocio.com>
2024-10-15 19:38:21 -0600

    feat: add gen1 generational dex

--- database/seeders/Gen1DexesSeeder.php
+++ database/seeders/Gen1DexesSeeder.php
@@ -17,11 +17,20 @@ class Gen1DexesSeeder extends Seeder
         $nationalDex = collect([
             ...Gen1Pokemon::cases(),
         ])
-            ->map(fn(Gen1Pokemon $pokemon) => [
-                'game_dex_id' => GameDex::GEN_1_NATIONAL->value,
-                'pokemon_id' => $pokemon->value,
-            ])->toArray();
+        ->map(fn(Gen1Pokemon $pokemon) => [
+            'game_dex_id' => GameDex::GEN_1_NATIONAL->value,
+            'pokemon_id' => $pokemon->value,
+        ])->toArray();
+
+        $generationalDex = collect([
+            ...Gen1Pokemon::cases(),
+        ])
+        ->map(fn(Gen1Pokemon $pokemon) => [
+            'game_dex_id' => GameDex::GEN_1_GENERATIONAL->value,
+            'pokemon_id' => $pokemon->value,
+        ])->toArray();
 
         DB::table('game_dex_pokemon')->insert($nationalDex);
+        DB::table('game_dex_pokemon')->insert($generationalDex);
     }
 }
